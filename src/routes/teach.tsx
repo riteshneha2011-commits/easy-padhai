6e70a5679f6f4fc74ac0b749c5503336d61cc565
Ritesh Agarwal <ritesh.bhopal@gmail.com>
2026-09-15 18:13:02 +0530

    fix: import Link in teach.tsx to resolve runtime reference error

--- src/routes/teach.tsx
+++ src/routes/teach.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
 import { useEffect, useState, useMemo } from "react";
